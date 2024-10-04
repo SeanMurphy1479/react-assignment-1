333e610606a75e7231e86324b7b15d2db789bc45
SeanMurphy1479 <seanm6077@gmail.com>
2024-10-04 16:04:44 +0100

    Movie card component created

--- movies/src/pages/homePage.js
+++ movies/src/pages/homePage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Grid from "@mui/material/Grid2";
+<<<<<<< HEAD
 
 const HomePage = (props) => {
 <<<<<<< HEAD
@@ -7,12 +8,24 @@ const HomePage = (props) => {
 =======
   //const movies = props.movies;
 >>>>>>> 12f25ef17baf7c0bc55f2521dfc693cb7c3bd194
+=======
+import MovieCard from '../components/movieCard'
+
+const HomePage = (props) => {
+  const movies = props.movies;
+>>>>>>> a2bdbd2 (Movie card component created)
     
   return (
   <Grid container>
       <Grid size={12}>
           <h1> HomePage </h1>
       </Grid>
+<<<<<<< HEAD
+=======
+      <Grid size={3}>
+        <MovieCard movie={movies[0]} />
+      </Grid>
+>>>>>>> a2bdbd2 (Movie card component created)
     </Grid>
   );
 };
