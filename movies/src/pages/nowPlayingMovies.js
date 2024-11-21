8e43902d1874d9f4b858f55c3fa0c5e66365642c
SeanMurphy1479 <seanm6077@gmail.com>
2024-11-21 19:23:03 +0000

    Added Pagination to Now_playing Movies

--- movies/src/pages/nowPlayingMovies.js
+++ movies/src/pages/nowPlayingMovies.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import PageTemplate from '../components/templateMovieListPage';
 import { getNowPlayingMovies } from "../api/tmdb-api";
 import AddToWatch from '../components/cardIcons/addToWatch';
 import Spinner from '../components/spinner';
 import { useQuery } from 'react-query';
+import { Pagination } from '@mui/material';
+
 
 const NowPlayingMovies = (props) => {
+    const [page, setPage] = useState(1);
+
 
     const { data, error, isLoading, isError } = useQuery('now_playing', getNowPlayingMovies)
 
@@ -19,13 +23,25 @@ const NowPlayingMovies = (props) => {
 
     const playing = data.results;
 
+    const handlePageChange = (event,value) => {
+        setPage(value);
+      }
+
     return (
+        <>
         <PageTemplate title='Now Playing Movies'
         movies={playing}
         action={(movie) => {
             return <AddToWatch movie={movie} />
         }}
-    />
+        />
+        <Pagination
+      count={data.total_pages}
+      page={page}
+      onChange={handlePageChange}
+      sx={{ display: "flex", justifyContent: "center"}}
+      />
+      </>
     )
 };
 export default NowPlayingMovies;
